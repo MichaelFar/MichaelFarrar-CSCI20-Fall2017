31627d8f15f407c49357300c815f490fa07e601b
Michael Farrar <mfarrar001@student.butte.edu>
2017-08-23 18:59:17 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,9 @@
+This algorithm is for the number guessing game.
+The program first generates a number at random between two numbers at random.
+The user is then prompted to answer the question "Guess a number between (num1) and (num2)"
+The program gives you ten chances and thus ten points possible, with each wrong guess being -1 point.
+when the user guesses the correct number the text "Congradulations! The number (numAns) was correct! Your score was (score)"
+The text "Would you like to play again?" will display after
+git add lab11
+git commit -a -m'Lab1.1 submission'
+git push
