890e0d4b007fae73e6a743ccfcb2a55b11301ca9
Michael Farrar <mfarrar001@student.butte.edu>
2017-08-28 19:05:31 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "                   , , , , , , , , , ," << endl;
+    cout << "                   | | | | | | | | | |" << endl;
+    cout << "           @%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@"<< endl;
+    cout << "           {               Happy                  }"<< endl;
+    cout << "           {               Birthday!              }"<< endl;
+    cout << "       @%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@"<<endl;
+    cout << "       {               				     }"<< endl;
+    cout << "       {               				     }"<< endl;
+    cout << "       {               				     }"<< endl;
+    cout << "      @%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@%@"<< endl;
+return 0;
+    
+}
+git add lab12;
+git commit -a -m'Lab12 Commit';
+git push;
