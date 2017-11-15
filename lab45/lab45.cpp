5bfc98d91748104b3c6b94a4a127db8be379521c
Michael Farrar <mfarrar001@student.butte.edu>
2017-11-15 20:48:14 +0000

    Lab 4.5

--- lab45/lab45.cpp
+++ lab45/lab45.cpp
@@ -1,14 +1,13 @@
-/*
-Michael Farrar
-11/13/17
+/*Michael Farrar
 csci 20
-lab 4.4
-April Browne
-
+11/15/17
+User name code
 */
+
 #include <iostream>
 #include <cstring>
 #include <string.h>
+#include <string>
 using namespace std;
 
 int main() {
@@ -21,83 +20,107 @@ int main() {
     int length_first_four = 0;
     int length_last_four = 0;
     int sessionEnd = 0;
-    
+    int i = 0;
     while (sessionEnd == 0)
     {
-        
         cout << "Please tell me your first name" << endl;
-        char userOneFirstName[11];
-        cin >> userOneFirstName;
+            string userOneFirstName;
+            cin >> userOneFirstName;
+        while (userOneFirstName.length() > 10)
+        {
+            if (userOneFirstName.length() > 10)
+            {
+                cout << "That first name is taken or is longer than ten characters, please enter another" << endl;
+                cin >> userOneFirstName;
+            }
+            
+        }
+        
         
         cout << "Please tell me your last name" << endl;
-        char userOneLastName[21];
+        string userOneLastName;
         cin >> userOneLastName;
+        while (userOneLastName.length() > 20)
+        {
+            if (userOneLastName.length() > 20)
+            {
+                cout << "That last name is taken or is longer than twenty characters, please enter another" << endl;
+                cin >> userOneLastName;
+            }
+            
+        }
         
         
         cout << "Please tell me your first name" << endl;
-        char userTwoFirstName[11];
+        string userTwoFirstName;
         cin >> userTwoFirstName;
-        while (strcmp (userOneFirstName, userTwoFirstName) == 0)//This function compares strings to eachother to see if they are the same, if they are the same it will ask to reinput it.
+        
+        while (userOneFirstName.length() > 10 || (userOneFirstName.compare(userTwoFirstName) == 0))//This function compares strings to eachother to see if they are the same, if they are the same it will ask to reinput it.
         {
-            cout << "That first name is taken, please enter another" << endl;
+            cout << "That first name is taken or is longer than ten characters, please enter another" << endl;
             cin >> userTwoFirstName;
             
             
         }
         cout << "Please tell me your last name" << endl;
-        char userTwoLastName[21];
+        string userTwoLastName;
         cin >> userTwoLastName;
         
         
-        while (strcmp (userOneLastName, userTwoLastName) == 0)
+        while (userOneLastName.length() > 20 || (userOneLastName.compare(userTwoLastName) == 0))
             {
                 cout << "That last name is taken, please enter another" << endl;
                 cin >> userTwoLastName;
             }
         cout << "Please tell me your first name" << endl;
-        char userThreeFirstName[11];
+        string userThreeFirstName;
         cin >> userThreeFirstName;
-        while ((strcmp (userThreeFirstName, userTwoFirstName) == 0) || (strcmp (userThreeFirstName, userOneFirstName) == 0))
+        while (userThreeFirstName.length() > 10 || (userThreeFirstName.compare(userTwoFirstName)) == 0 || (userThreeFirstName.compare(userOneFirstName) == 0))
         {
-            cout << "That first name is taken, please enter another" << endl;
+            cout << "That first name is taken or is longer than ten characters, please enter another" << endl;
             cin >> userThreeFirstName;
             
             
         }
         cout << "Please tell me your last name" << endl;
-        char userThreeLastName[21];
+        string userThreeLastName;
         cin >> userThreeLastName;
-        while ((strcmp (userThreeLastName, userTwoLastName) == 0) || (strcmp (userThreeLastName, userOneLastName) == 0))
+        while (userThreeLastName.length() > 20 || (userThreeLastName.compare(userTwoLastName) == 0) || (userThreeLastName.compare(userOneLastName) == 0))
             {
                 cout << "That last name is taken, please enter another" << endl;
                 cin >> userThreeLastName;
             }
         cout << "Please tell me your first name" << endl;
-        char userFourFirstName[11];
+        string userFourFirstName;
         cin >> userFourFirstName;
-        while ((strcmp (userFourFirstName, userThreeFirstName) == 0) || (strcmp (userFourFirstName, userTwoFirstName) == 0) || (strcmp (userFourFirstName, userOneFirstName) == 0))
+        while (userFourFirstName.length() > 10 || (userFourFirstName.compare(userThreeFirstName) == 0) || (userFourFirstName.compare(userTwoFirstName) == 0) || (userFourFirstName.compare(userOneFirstName) == 0))
         {
-            cout << "That first name is taken, please enter another" << endl;
+            cout << "That first name is taken or is longer than ten characters, please enter another" << endl;
             cin >> userFourFirstName;
             
             
         }
         cout << "Please tell me your last name" << endl;
-        char userFourLastName[21];
+        string userFourLastName;
         cin >> userFourLastName;
-        while ((strcmp (userFourLastName, userThreeLastName) == 0) || (strcmp (userFourLastName, userTwoLastName) == 0) || (strcmp (userFourLastName, userOneLastName) == 0))
+        
+        while (userFourLastName.length() > 20 || (userFourLastName.compare(userThreeLastName) == 0) || (userFourLastName.compare(userTwoLastName) == 0) || (userFourLastName.compare(userOneLastName) == 0))
             {
                 cout << "That last name is taken, please enter another" << endl;
                 cin >> userFourLastName;
             }
-        length_first_one = strlen(userOneFirstName);//These take the lenngth of the strings and put them in the for loop
-        length_last_one = strlen(userOneLastName);
-        length_first_two = strlen(userTwoFirstName);
-        length_last_two = strlen(userTwoLastName);
-        length_first_three = strlen(userThreeFirstName);
-        length_last_three = strlen(userThreeLastName);
-        length_first_four = strlen(userFourFirstName);
-        length_last_four = strlen(userFourLastName);
+        
+        
+        
+        
+        length_first_one = userOneFirstName.length();//These take the lenngth of the strings and put them in the for loop
+        length_last_one = userOneLastName.length();
+        length_first_two = userTwoFirstName.length();
+        length_last_two = userTwoLastName.length();
+        length_first_three = userThreeFirstName.length();
+        length_last_three = userThreeLastName.length();
+        length_first_four = userFourFirstName.length();
+        length_last_four = userFourLastName.length();
         for(int i = 0 ; i < length_first_one ; i ++ ){
       cout << userOneFirstName[i];
         }
@@ -153,3 +176,66 @@ int main() {
     }
         
     }
+
+/*
+Please tell me your first name
+Michael
+Please tell me your last name
+Farrar
+Please tell me your first name
+April
+Please tell me your last name
+Browne
+Please tell me your first name
+AnaElizabeth
+That first name is taken or is longer than ten characters, please enter another
+Ana
+Please tell me your last name
+Hazeltine-Smith
+Please tell me your first name
+James
+Please tell me your last name
+James
+Michael Farrar
+Choose one: 
+
+MiFarrar
+MichaelFarrar
+MFarrar
+
+
+ April Browne
+Choose one: 
+
+ApBrowne
+AprilBrowne
+ABrowne
+
+
+ Ana 
+Hazeltine-Smith
+Choose one: 
+
+AnHazeltine-Smith
+AnaHazeltine-Smith
+AHazeltine-Smith
+
+
+ James James
+Choose one: 
+
+JaJames
+JamesJames
+JJames
+
+
+Quit? 1 is yes, 0 is no
+
+
+
+
+
+
+
+
+*/
